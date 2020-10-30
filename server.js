9413e5b0cfd401d7ff8b64a04a3331c12ec14a46
bhaskarjha514 <bhaskarjha514@gmail.com>
2020-10-31 04:15:12 +0530

    Added cors

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json({ extended : true }))
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","*")
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+    next();
+})
 
 // connect to database
 connectDB()
